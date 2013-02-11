41ec01c2592a726925c2e8823f4c6fd9668d5cc4
Oliver <Oliver.korfitsen@gmail.com>
2013-02-11 12:00:53 +0100

    Ny metode

--- KorselsFradrag_School/src/kf/KF.java
+++ KorselsFradrag_School/src/kf/KF.java
@@ -13,9 +13,19 @@ public class KF {
         kFGUI.setVisible(true);
     }
 
-    public static double calculate() {
-        //temp
-        return 0;
+    public static double calculate(int kilometer) {
+        if (kilometer < 120) {
+            return (kilometer - 24) * 1.05;
+
+        } else if (kilometer > 24) {
+            return (kilometer - 24) * 2.10;
+
+
+        } else {
+            return 0;
+        }
+
+
     }
 
     public static void main(String args[]) {
