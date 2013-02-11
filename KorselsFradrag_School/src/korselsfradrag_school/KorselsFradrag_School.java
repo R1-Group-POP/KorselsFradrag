6a2f26997734b99c2b6d2fd80a65a408a0fee3a1
Patrick <pkkann@gmail.com>
2013-02-11 11:10:39 +0100

    Project start

---
+++ KorselsFradrag_School/src/korselsfradrag_school/KorselsFradrag_School.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package korselsfradrag_school;
+
+/**
+ *
+ * @author Patrick
+ */
+public class KorselsFradrag_School {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
