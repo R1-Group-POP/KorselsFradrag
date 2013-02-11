2ed92cf0f78de1226a36ca60440a48f12cd41701
Patrick <pkkann@gmail.com>
2013-02-11 11:37:54 +0100

    2

---
+++ KorselsFradrag_School/src/kf/KF.java
@@ -0,0 +1,24 @@
+package kf;
+
+/**
+ *
+ * @author Patrick
+ */
+public class KF {
+
+    private KFGUI kFGUI;
+
+    public KF() {
+        kFGUI = new KFGUI();
+        kFGUI.setVisible(true);
+    }
+
+    public static double calculate() {
+        //temp
+        return 0;
+    }
+
+    public static void main(String args[]) {
+        new KF();
+    }
+}
